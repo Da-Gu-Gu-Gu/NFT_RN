ef319aabf25b92d1ebdbdb9257d653a4de580f1e
Da-Gu-Gu-Gu <appeton54@gmail.com>
2022-07-29 16:27:29 +0630

    first commit

---
+++ components/index.js
@@ -0,0 +1,18 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import { CircleButton,RectButton } from "./Button";
+import DetailBid from "./DetailBid";
+import DetailDesc from "./DetailDesc";
+import { SubInfo } from "./SubInfo";
+
+export {
+    HomeHeader,
+    FocusedStatusBar,
+    NFTCard,
+    CircleButton,
+    RectButton,
+    DetailDesc,
+    DetailBid,
+    SubInfo
+}
